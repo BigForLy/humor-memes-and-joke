f3e9963e74e455e4da4215501837efa1940704e8
BigForLy <oringan@mail.ru>
2021-11-08 01:23:37 +0500

    #5

--- app/src/main/java/com/example/humormemeandjoke/MainActivity.kt
+++ app/src/main/java/com/example/humormemeandjoke/MainActivity.kt
@@ -3,23 +3,16 @@ package com.example.humormemeandjoke
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.HumorMemeAndJoke_NoActionBar)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        setButtonActivity()
-    }
-
-    private fun setButtonActivity() {
-        val startButton = findViewById<Button>(R.id.buttonToStart)
-        startButton.setOnClickListener{
-            startButton.isClickable = false
-
-            val chefIntent = Intent(this, StartHumorActivity::class.java)
-            startActivity(chefIntent)
-        }
+        val chefIntent = Intent(this, StartHumorActivity::class.java)
+        startActivity(chefIntent)
+        finish()
     }
 }
