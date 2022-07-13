9caf31de71d444c02fdd6bb08882f50f99f1efbd
BigForLy <oringan@mail.ru>
2022-07-13 23:25:43 +0500

    patch: updated app name

--- app/src/main/java/com/example/humormemeandjoke/MainActivity.kt
+++ app/src/main/java/com/example/humormemeandjoke/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.humormemeandjoke
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
